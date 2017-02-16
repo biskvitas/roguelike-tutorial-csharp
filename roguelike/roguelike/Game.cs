70ebaee1a788653665fab70915244be18f7fd6a9
Vitalij Spakov <bigvit2@gmail.com>
2017-02-16 21:06:44 +0000

    Updated Console Window
    Added colours and split main console into sub-windows

--- roguelike/roguelike/Game.cs
+++ roguelike/roguelike/Game.cs
@@ -1,15 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using RLNET;
 
 namespace roguelike
 {
     class Program
     {
         static void Main(string[] args)
+
+
+        // Event handler for RLNET's Render event
+        private static void OnRootConsoleRender(object sender, UpdateEventArgs e)
         {
+            // Blit the sub consoles to the root console in the correct locations
+            RLConsole.Blit(_mapConsole, 0, 0, _mapWidth, _mapHeight, _rootConsole, 0, _inventoryHeight);
+            RLConsole.Blit(_statConsole, 0, 0, _statWidth, _statHeight, _rootConsole, _mapWidth, 0);
+            RLConsole.Blit(_messageConsole, 0, 0, _messageWidth, _messageHeight, _rootConsole, 0, _screenHeight - _messageHeight);
+            RLConsole.Blit(_inventoryConsole, 0, 0, _inventoryWidth, _inventoryHeight, _rootConsole, 0, 0);
+
+            // Tell RLNET to draw the console that we set
+            _rootConsole.Draw();
         }
     }
 }
