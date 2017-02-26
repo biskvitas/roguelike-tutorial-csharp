a0bd10e0587c69b2c51c3912acfdf61bcea8c8b0
Vitalij Spakov <bigvit2@gmail.com>
2017-02-26 21:35:15 +0000

    Added field of view
    Added code to calculate players field of view and show/hide tiles appropriatelly

--- roguelike/roguelike/MapObjects/Floor.cs
+++ roguelike/roguelike/MapObjects/Floor.cs
@@ -3,11 +3,25 @@ using SadConsole;
 
 namespace roguelike.MapObjects
 {
-    public class Floor : CellAppearance 
+    public class Floor : MapObjectBase 
     {
-        public Floor() : base(Color.DarkGray, Color.Transparent, 46)
-        {
+        public Floor() : base(Color.DarkGray, Color.Transparent, 46) {}
 
+        public override void RenderToCell(Cell sadConsoleCell, bool isFov, bool isExplored)
+        {
+            base.RenderToCell(sadConsoleCell, isFov, isExplored);
+
+            if (isFov)
+            {
+                sadConsoleCell.GlyphIndex = 46;
+            }
+        }
+
+        public override void RemoveCellFromView(Cell sadConsoleCell)
+        {
+            base.RemoveCellFromView(sadConsoleCell);
+
+            sadConsoleCell.GlyphIndex = 0;
         }
     }
 }
---
+++ roguelike/roguelike/MapObjects/MapObjectBase.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.Xna.Framework;
+using SadConsole;
+using SadConsole.Effects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace roguelike.MapObjects
+{
+    public abstract class MapObjectBase
+    {
+        public CellAppearance Appearance { get; set; }
+        public ICellEffect EffectSeen { get; set; }
+        public ICellEffect EffectHidden { get; set; }
+
+        public MapObjectBase(Color foreground, Color background, int character)
+        {
+            Appearance = new CellAppearance(foreground, background, character);
+
+            EffectSeen = new Recolor()
+            {
+                Foreground = Color.LightGray * 0.3f,
+                Background = Color.LightGray * 0.3f,
+                DoForeground = true,
+                DoBackground = true,
+                CloneOnApply = false
+            };
+
+            EffectHidden = new Recolor()
+            {
+                Foreground = Color.Black,
+                Background = Color.Black,
+                DoForeground = true,
+                DoBackground = true,
+                CloneOnApply = false
+            };
+        }
+
+        public virtual void RenderToCell(SadConsole.Cell sadConsoleCell, bool isFov, bool isExplored)
+        {
+            Appearance.CopyAppearanceTo(sadConsoleCell);
+
+            // Clear out the old effect if there was one
+            if (sadConsoleCell.Effect != null)
+            {
+                sadConsoleCell.Effect.Clear(sadConsoleCell);
+                sadConsoleCell.Effect = null;
+            }
+
+            if (isFov)
+            {
+                // Do nothing if it's in view, it's a normal colored square
+                // You could do something later like check how far the cell is from the player and tint it
+            }
+            else if (isExplored)
+            {
+                sadConsoleCell.Effect = EffectSeen;
+                sadConsoleCell.Effect.Apply(sadConsoleCell);
+            }
+            else
+            {
+                sadConsoleCell.Effect = EffectHidden;
+                sadConsoleCell.Effect.Apply(sadConsoleCell);
+            }
+        }
+
+
+        public virtual void RemoveCellFromView(SadConsole.Cell sadConsoleCell)
+        {
+            // Clear out the old effect if there was one
+            if (sadConsoleCell.Effect != null)
+            {
+                sadConsoleCell.Effect.Clear(sadConsoleCell);
+                sadConsoleCell.Effect = null;
+            }
+
+            sadConsoleCell.Effect = EffectSeen;
+            sadConsoleCell.Effect.Apply(sadConsoleCell);
+        }
+    }
+}
